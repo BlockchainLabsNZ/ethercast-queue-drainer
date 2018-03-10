6384c4113c018878fde4d91ef84b77d1b3a396bd
Moody Salem <moody.salem@gmail.com>
2018-03-10 15:10:18 -0500

    add docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,197 @@
+ï»¿var typedoc = typedoc || {};
+typedoc.search = typedoc.search || {};
+typedoc.search.data = {
+  "kinds": {
+    "128": "Class",
+    "256": "Interface",
+    "512": "Constructor",
+    "1024": "Property",
+    "2048": "Method",
+    "65536": "Type literal",
+    "4194304": "Type alias"
+  },
+  "rows": [{
+    "id": 0,
+    "kind": 256,
+    "name": "QueueDrainerConstructorOptions",
+    "url": "interfaces/queuedrainerconstructoroptions.html",
+    "classes": "tsd-kind-interface"
+  }, {
+    "id": 1,
+    "kind": 1024,
+    "name": "logger",
+    "url": "interfaces/queuedrainerconstructoroptions.html#logger",
+    "classes": "tsd-kind-property tsd-parent-kind-interface",
+    "parent": "QueueDrainerConstructorOptions"
+  }, {
+    "id": 2,
+    "kind": 1024,
+    "name": "sqs",
+    "url": "interfaces/queuedrainerconstructoroptions.html#sqs",
+    "classes": "tsd-kind-property tsd-parent-kind-interface",
+    "parent": "QueueDrainerConstructorOptions"
+  }, {
+    "id": 3,
+    "kind": 1024,
+    "name": "queueUrl",
+    "url": "interfaces/queuedrainerconstructoroptions.html#queueurl",
+    "classes": "tsd-kind-property tsd-parent-kind-interface",
+    "parent": "QueueDrainerConstructorOptions"
+  }, {
+    "id": 4,
+    "kind": 1024,
+    "name": "handleMessage",
+    "url": "interfaces/queuedrainerconstructoroptions.html#handlemessage",
+    "classes": "tsd-kind-property tsd-parent-kind-interface",
+    "parent": "QueueDrainerConstructorOptions"
+  }, {
+    "id": 5,
+    "kind": 1024,
+    "name": "shouldContinue",
+    "url": "interfaces/queuedrainerconstructoroptions.html#shouldcontinue",
+    "classes": "tsd-kind-property tsd-parent-kind-interface",
+    "parent": "QueueDrainerConstructorOptions"
+  }, {
+    "id": 6,
+    "kind": 1024,
+    "name": "batchSize",
+    "url": "interfaces/queuedrainerconstructoroptions.html#batchsize",
+    "classes": "tsd-kind-property tsd-parent-kind-interface",
+    "parent": "QueueDrainerConstructorOptions"
+  }, {
+    "id": 7,
+    "kind": 1024,
+    "name": "stopWhenEmpty",
+    "url": "interfaces/queuedrainerconstructoroptions.html#stopwhenempty",
+    "classes": "tsd-kind-property tsd-parent-kind-interface",
+    "parent": "QueueDrainerConstructorOptions"
+  }, {
+    "id": 8,
+    "kind": 128,
+    "name": "QueueDrainer",
+    "url": "classes/queuedrainer.html",
+    "classes": "tsd-kind-class"
+  }, {
+    "id": 9,
+    "kind": 1024,
+    "name": "logger",
+    "url": "classes/queuedrainer.html#logger",
+    "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 10,
+    "kind": 1024,
+    "name": "sqs",
+    "url": "classes/queuedrainer.html#sqs",
+    "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 11,
+    "kind": 1024,
+    "name": "queueUrl",
+    "url": "classes/queuedrainer.html#queueurl",
+    "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 12,
+    "kind": 1024,
+    "name": "handleMessage",
+    "url": "classes/queuedrainer.html#handlemessage",
+    "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 13,
+    "kind": 1024,
+    "name": "shouldContinue",
+    "url": "classes/queuedrainer.html#shouldcontinue",
+    "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 14,
+    "kind": 1024,
+    "name": "batchSize",
+    "url": "classes/queuedrainer.html#batchsize",
+    "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 15,
+    "kind": 1024,
+    "name": "stopWhenEmpty",
+    "url": "classes/queuedrainer.html#stopwhenempty",
+    "classes": "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 16,
+    "kind": 2048,
+    "name": "deleteMessages",
+    "url": "classes/queuedrainer.html#deletemessages",
+    "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 17,
+    "kind": 2048,
+    "name": "processMessages",
+    "url": "classes/queuedrainer.html#processmessages",
+    "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 18,
+    "kind": 512,
+    "name": "constructor",
+    "url": "classes/queuedrainer.html#constructor",
+    "classes": "tsd-kind-constructor tsd-parent-kind-class",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 19,
+    "kind": 2048,
+    "name": "drain",
+    "url": "classes/queuedrainer.html#drain",
+    "classes": "tsd-kind-method tsd-parent-kind-class",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 20,
+    "kind": 2048,
+    "name": "poll",
+    "url": "classes/queuedrainer.html#poll",
+    "classes": "tsd-kind-method tsd-parent-kind-class tsd-is-private",
+    "parent": "QueueDrainer"
+  }, {
+    "id": 21,
+    "kind": 4194304,
+    "name": "Message",
+    "url": "globals.html#message",
+    "classes": "tsd-kind-type-alias"
+  }, {
+    "id": 22,
+    "kind": 4194304,
+    "name": "MessageHandler",
+    "url": "globals.html#messagehandler",
+    "classes": "tsd-kind-type-alias"
+  }, {
+    "id": 23,
+    "kind": 65536,
+    "name": "__type",
+    "url": "globals.html#messagehandler.__type",
+    "classes": "tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported",
+    "parent": "MessageHandler"
+  }, {
+    "id": 24,
+    "kind": 4194304,
+    "name": "ShouldContinueFunction",
+    "url": "globals.html#shouldcontinuefunction",
+    "classes": "tsd-kind-type-alias"
+  }, {
+    "id": 25,
+    "kind": 65536,
+    "name": "__type",
+    "url": "globals.html#shouldcontinuefunction.__type",
+    "classes": "tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported",
+    "parent": "ShouldContinueFunction"
+  }, {
+    "id": 26,
+    "kind": 4194304,
+    "name": "BatchSize",
+    "url": "globals.html#batchsize",
+    "classes": "tsd-kind-type-alias"
+  }]
+};
